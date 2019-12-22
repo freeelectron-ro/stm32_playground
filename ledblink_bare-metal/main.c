ea70123c573646471d678089af97d08c4be51cd2
brix <grig@freeelectron.ro>
2019-12-22 17:18:30 +0200

    led blink

--- ledblink_bare-metal/main.c
+++ ledblink_bare-metal/main.c
@@ -1,4 +1,32 @@
-void main ()
+#include <stdint.h>
+
+// register address
+#define RCC_BASE      0x40021000
+#define GPIOC_BASE    0x40011000
+#define RCC_APB2ENR   *(volatile uint32_t *)(RCC_BASE   + 0x18)
+#define GPIOC_CRH     *(volatile uint32_t *)(GPIOC_BASE + 0x04)
+#define GPIOC_ODR     *(volatile uint32_t *)(GPIOC_BASE + 0x0C)
+
+// bit fields
+#define RCC_IOPCEN   (1<<4)
+#define GPIOC13      (1UL<<13)
+
+
+void main(void)
 {
-    while (1);
+	RCC_APB2ENR |= RCC_IOPCEN;
+	GPIOC_CRH   &= 0xFF0FFFFF;
+	GPIOC_CRH   |= 0x00200000;
+
+	while(1)
+	{
+		GPIOC_ODR |=  GPIOC13;
+		for (int i = 0; i < 500000; i++); // arbitrary delay
+		GPIOC_ODR &= ~GPIOC13;
+		for (int i = 0; i < 500000; i++); // arbitrary delay
+	}
 }
+
+
+
+	
